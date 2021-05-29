b7c4d7ed4b417463a6f43b7dd0b7f757d375dc07
PrathamRawat <prathamrawat815@gmail.com>
2021-05-29 00:42:06 -0400

    Set up basic project structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './app/router/routes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <DndProvider backend={HTML5Backend}>
+        <Router>
+          <Routes />
+        </Router>
+      </DndProvider>
   );
 }
 
