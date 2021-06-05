c0be2cc49a0af079b3dc67534c2a871ea0a5e6b7
PrathamRawat <prathamrawat815@gmail.com>
2021-06-05 13:17:50 -0400

    Add function to get a variable number of search results

--- src/app/utils/requests.ts
+++ src/app/utils/requests.ts
@@ -4,22 +4,39 @@ import axios from "axios";
 const URL = "http://www.omdbapi.com/";
 const POSTER_URL = "http://img.omdbapi.com/";
 
+export interface SearchResult {
+    Title: string,
+    Type: string,
+    Year: string,
+    imdbID: string,
+    poster: string,
+}
+
 export const getPoster = async (title: string) => {
-    let response = await axios.get(POSTER_URL, {
+    let response = (await axios.get(POSTER_URL, {
         params: {
             apikey: API_KEY,
             t: title,
+        }
+    })).data
+    return response
+}
+
+export const getSearchResults = async (query: string, pageNumber: number) => {
+    let response = await axios.get(URL, {
+        params: {
+            apikey: API_KEY,
+            s: query,
+            page: pageNumber,
         }
     })
     return response.data
 }
 
-export const getSearchResults = async (query: string) => {
-    let response = await axios.get(URL, {
-        params: {
-            apikey: API_KEY,
-            s: query,
-        }
-    })
-    return response.data
+export const searchResults = async (query: string, pageNumber: number, rowsPerPage: number) => {
+    let output: never[] = []
+    for (let i = 1; i <= rowsPerPage / 10; i++) {
+        output = output.concat((await getSearchResults(query, (rowsPerPage / 10) * (pageNumber + i))).Search)
+    }
+    return output
 }
